dcbe4d225a1fda26b49957f97f57da619d70042d
Lizzie <leejihye7117@gmail.com>
2023-07-11 23:02:22 +0900

    chatbot still 404

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,76 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import React, { useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
 
-export default function Home() {
+const Wrapper = styled.div`
+  .chat-content {
+    height: 350px;
+    overflow-y: scroll;
+  }
+  .line {
+    margin-top: 15px;
+    display: flex;
+  }
+  .chat-box {
+    background: #eee;
+    padding: 5px;
+    max-width: 200px;
+  }
+  .mine {
+    margin-left: auto;
+  }
+`;
+
+const ChatApp = () => {
+  const [messages, setMessages] = useState([
+    { content: "안녕?", isMine: false },
+    { content: "안녕?", isMine: true },
+  ]);
+  const [inputValue, setInputValue] = useState("");
+
+  const sendMessage = () => {
+    const newMessage = { content: inputValue, isMine: true };
+    setMessages((prevMessages) => [...prevMessages, newMessage]);
+    setInputValue("");
+
+    const query = `http://localhost:3000/translate?q=${inputValue}`;
+    axios
+      .get(query)
+      .then((response) => {
+        const translatedText = JSON.parse(response.data).message.result
+          .translatedText;
+        console.log(translatedText);
+        const translatedMessage = { content: translatedText, isMine: false };
+        setMessages((prevMessages) => [...prevMessages, translatedMessage]);
+      })
+      .catch((error) => {
+        console.log("실패", error);
+      });
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <Wrapper>
+      <div className="chat-content">
+        {messages.map((message, index) => (
+          <div className="line" key={index}>
+            <div className={`chat-box ${message.isMine ? "mine" : ""}`}>
+              {message.content}
+            </div>
+          </div>
+        ))}
       </div>
+      <input
+        className="chat-box"
+        id="input"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button id="send" onClick={sendMessage}>
+        전송
+      </button>
+    </Wrapper>
+  );
+};
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default ChatApp;
